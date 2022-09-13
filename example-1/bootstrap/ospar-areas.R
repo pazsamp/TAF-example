7628f8a93a4f70a2bcbf1116890f53eb9ad4f0af
pazsamp <paz.sampedro@co.ieo.es>
2022-09-13 15:36:22 +0200

    Update exampl1

---
+++ example-1/bootstrap/ospar-areas.R
@@ -0,0 +1,19 @@
+library(icesTAF)
+library(sf)
+
+download(
+  "https://gis.ices.dk/shapefiles/OSPAR_Subregions.zip"
+)
+
+unzip("OSPAR_Subregions.zip")
+unlink("OSPAR_Subregions.zip")
+
+areas <- st_read("OSPAR_subregions_20160418_3857.shp")
+
+# write as csv
+st_write(
+  areas, "ospar-areas.csv",
+  layer_options = "GEOMETRY=AS_WKT"
+)
+
+unlink(dir(pattern = "OSPAR_subregions_20160418_3857"))
